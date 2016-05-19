b509c189ba10a862ae0b8e229c58e15bea20884f
Parker Agee <pagee@3442452.local>
2016-05-19 12:05:25 -0500

    Quick updates

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactRouter = require('react-router');
+var Router  = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Navigation = ReactRouter.Navigation;
+var createBrowserHistory = require('history/lib/createBrowserHistory');
+
+
+var App = React.createClass({
+
+  render : function() {
+    return (
+      <div className="container">
+        Hello
+      </div>
+    )
+  }
+});
+
+/*
+  Routes
+*/
+
+var routes = (
+  <Router history={createBrowserHistory()}>
+    <Route path="/" component={App}/>
+  </Router>
+)
+
+ReactDOM.render(routes, document.querySelector('#main'));
